05bb1139e997149b523d02779072d8f6c74111c2
Demetrio Aris <gon21010@byui.edu>
2024-08-08 20:42:26 -0500

    Update media Query

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const courses = [
     {
+        //Web and Computer Programming certificate
         subject: 'CSE',
         number: 110,
         title: 'Introduction to Programming',
@@ -39,7 +40,7 @@ const courses = [
         certificate: 'Web and Computer Programming',
         description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call, debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
         technology: ['Python'],
-        completed: false
+        completed: true
     },
     {
         subject: 'CSE',
@@ -59,7 +60,7 @@ const courses = [
         certificate: 'Web and Computer Programming',
         description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
         technology: ['HTML', 'CSS', 'JavaScript'],
-        completed: false
+        completed: true
     },
     {
         subject: 'WDD',
@@ -70,15 +71,76 @@ const courses = [
         description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
         technology: ['HTML', 'CSS', 'JavaScript'],
         completed: false
-    }
+    },
+    //Web Development certificate
+    {
+        subject: 'ITM',
+        number: 111,
+        title: 'Introduction to Databases',
+        credits: 3,
+        certificate: 'Web Development',
+        description: 'This course covers the basic elements of database management systems. It introduces students to the concepts of logical and physical relationships in a data model and the concepts of inner and outer joins. Students will use a computer aided software engineering (CASE) tool to design, create, and query a database.',
+        technology: ['Data Base', 'MySQL'],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 330,
+        title: 'Web Frontend Development II',
+        credits: 3,
+        certificate: 'Web Development',
+        description: 'This course will continue with the topics presented in WDD 231 Web Front-end Development I: Building websites with HTML, CSS, and Javascript. This course will have a stronger emphasis on Javascript development and mobile design as students create mobile web applications.',
+        technology: ['HTML', 'CSS', 'JavaScript'],
+        completed: false
+    },
+    {
+        subject: 'CSE',
+        number: 340,
+        title: 'Web Backend Development',
+        credits: 3,
+        certificate: 'Web Development',
+        description: 'This programming course focuses on constructing dynamic web sites using server-side languages, making use of databases and design patterns. The concepts introduced in Web Frontend Development courses are expected to be continued and implemented.',
+        technology: ['Server Side'],
+        completed: false
+    },
+    {
+        subject: 'CSE',
+        number: 341,
+        title: 'Web Services',
+        credits: 3,
+        certificate: 'Web Development',
+        description: 'This course focuses on the backend development of dynamic, service-oriented web applications. Students will learn how to design and implement web services, how to interact with data storage, and how to use these tools to build functioning web applications.',
+        technology: ['Server Side'],
+        completed: false
+    },
+    {
+        subject: 'WDD',
+        number: 430,
+        title: 'Web Full-Stack Development',
+        credits: 3,
+        certificate: 'Web Development',
+        description: 'This course will teach you how to design and build interactive web based applications using HTML, CSS, JavaScript, and a web development stack.',
+        technology: ['Server Side'],
+        completed: false
+    },
+
+    //Software Development certificate
 ];
 
 function output(courses) {
-    const boxCourse = document.querySelector('.boxCourse');
+    const certificate01 = document.querySelector('.boxcertificate01');
+    const certificate02 = document.querySelector('.boxcertificate02'); 
+    const certificate03 = document.querySelector('.boxcertificate03'); 
+
+    // Inicializar las variables totalCredits para cada certificado.
+    let totalCreditsCert01 = 0;
+    let totalCreditsCert02 = 0;
+    let totalCreditsCert03 = 0;
+
     courses.forEach((course) => {
         let courseDiv = document.createElement('div');
         courseDiv.classList.add('course');
-        
+
         if (course.completed) {
             courseDiv.classList.add('courseComplete');
         } else {
@@ -86,18 +148,32 @@ function output(courses) {
         }
         courseDiv.id = 'course';
         courseDiv.setAttribute('data-subject', course.subject);
-        
+
         let courseTitle = document.createElement('h2');
         courseTitle.textContent = `${course.subject} ${course.number}`;
-    
+
         courseDiv.appendChild(courseTitle);
-        boxCourse.appendChild(courseDiv);
+
+        // Clasificar cursos por certificado y agregar los créditos correspondientes.
+        if (course.certificate == 'Web and Computer Programming') {
+            certificate01.appendChild(courseDiv);
+            totalCreditsCert01 += course.credits;
+        } else if (course.certificate == 'Web Development') { 
+            certificate02.appendChild(courseDiv);
+            totalCreditsCert02 += course.credits;
+        } else if (course.certificate == 'Another Certificate Name 2') { 
+            certificate03.appendChild(courseDiv);
+            totalCreditsCert03 += course.credits;
+        }
     });
 
-    const totalCredits = courses.reduce((sum, course) => sum + course.credits, 0);
-    document.getElementById('totalCredits').textContent = `Total Credits: ${totalCredits}`;
-    //make chages
+    // Mostrar el total de créditos para cada certificado.
+    document.getElementById('totalCreditsCert01').textContent = `Total Credits: ${totalCreditsCert01}`;
+    document.getElementById('totalCreditsCert02').textContent = `Total Credits: ${totalCreditsCert02}`;
+    document.getElementById('totalCreditsCert03').textContent = `Total Credits: ${totalCreditsCert03}`;
 }
+
+
 
 output(courses);
 
@@ -106,7 +182,7 @@ const boxButtons = document.querySelectorAll('.boxButton button');
 
 boxButtons.forEach(button => {
     button.addEventListener("click", function () {
-        const selectButton = this.value.toUpperCase(); 
+        const selectButton = this.value.toUpperCase();
         if (selectButton === "ALL") {
             boxCourse.querySelectorAll("#course").forEach(function (course) {
                 course.style.display = "block";
