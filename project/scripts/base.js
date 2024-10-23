d62ee9c9001a858d3f1d793e9ac8439fee4102e4
Demetrio Aris <gon21010@byui.edu>
2024-10-22 21:54:07 -0500

    spring-01

--- project/scripts/base.js
+++ project/scripts/base.js
@@ -1,14 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuButton = document.getElementById('menu');
-    const navigation = document.querySelector('.navigation');
-    
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.getElementById("menu");
+    const navigation = document.querySelector("nav");
 
-    menuButton.addEventListener('click', () => {
-        menuButton.classList.toggle('open');
-        navigation.classList.toggle('open');
+    menuButton.addEventListener("click", () => {
+        menuButton.classList.toggle("open");
+        navigation.classList.toggle("open");
     });
 
-
-    document.getElementById('currentyear').textContent = new Date().getFullYear();
-    document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+    document.getElementById("currentyear").textContent =
+        new Date().getFullYear();
+    document.getElementById(
+        "lastModified"
+    ).textContent = `Last Modified: ${document.lastModified}`;
 });
